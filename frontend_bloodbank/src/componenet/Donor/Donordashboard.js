171c5605c35290fe7e8535d87da445d5bc942845
Krishna Jade <krishnajade222@gmail.com>
2022-09-22 23:38:47 +0530

    Donor spelling correction

--- frontend_bloodbank/src/componenet/Donor/Donordashboard.js
+++ frontend_bloodbank/src/componenet/Donor/Donordashboard.js
@@ -1,36 +1,32 @@
 import React, { useState, useEffect } from "react";
 import { Container } from "react-bootstrap";
-import './dashboard.css';
-function Donardashboard() {
-  const [Donarlist, setDonarlist] = useState([]);
+import "./dashboard.css";
+function Donordashboard() {
+  const [Donorlist, setDonorlist] = useState([]);
 
   useEffect(() => {
-    const getDonarlist = async () => {
+    const getDonorlist = async () => {
       const res = await fetch("http://localhost:8080/getallDonor");
       const getdata = await res.json();
-      setDonarlist(getdata);
+      setDonorlist(getdata);
       // console.log(getdata);
     };
 
-    getDonarlist();
-  },[]);
-
+    getDonorlist();
+  }, []);
 
   return (
     <React.Fragment>
       <Container>
-        <div className="row" >
-           
+        <div className="row">
           <div className="col-sm-8 text-success">
-            <h5 className="p-3 fw-bold text-black">
-              Donar Details
-            </h5>
-          
-            <table className="table table-bordered text-white" >
+            <h5 className="p-3 fw-bold text-black">Donor Details</h5>
+
+            <table className="table table-bordered text-white">
               <thead>
                 <tr>
-                  <th>Donar ID</th>
-                  <th>Donar Name</th>
+                  <th>Donor ID</th>
+                  <th>Donor Name</th>
                   <th>Address</th>
                   <th>City</th>
                   <th>Blood group</th>
@@ -39,24 +35,29 @@ function Donardashboard() {
                 </tr>
               </thead>
               <tbody>
-                {Donarlist.map((donar1) => (
-                    <tr>
-                    <td>{donar1.donar_id}</td>
-                    <td>{donar1.address}</td>
-                    <td>{donar1.bdate}</td>
-                    <td>{donar1.city}</td>
-                    <td>{donar1.email}</td>
-                    
-                    <td><button href="" className="btn btn-success"> View </button> </td>
+                {Donorlist.map((Donor1) => (
+                  <tr>
+                    <td>{Donor1.Donor_id}</td>
+                    <td>{Donor1.address}</td>
+                    <td>{Donor1.bdate}</td>
+                    <td>{Donor1.city}</td>
+                    <td>{Donor1.email}</td>
+
+                    <td>
+                      <button href="" className="btn btn-success">
+                        {" "}
+                        View{" "}
+                      </button>{" "}
+                    </td>
                   </tr>
                 ))}
               </tbody>
             </table>
           </div>
-        </div>      
+        </div>
       </Container>
     </React.Fragment>
   );
 }
 
-export default Donardashboard;
+export default Donordashboard;
